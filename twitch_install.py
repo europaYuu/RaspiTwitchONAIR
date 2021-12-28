0fece44763c9661cdf67e654992e77cd1aeeb232
Europa Yuu <92567050+europaYuu@users.noreply.github.com>
2021-12-27 21:27:57 -0800

    WIP: add easy install / update scripts

---
+++ twitch_install.py
@@ -0,0 +1,48 @@
+import os
+
+path = os.getcwd()
+
+print('\n/////////////////////////////////')
+print('Installing Twitch ON AIR Services...')
+print('/////////////////////////////////')
+print(' ')
+print('Current Path: ' + path)
+
+print('\n/////////////////////////////////')
+print('Installing Dependencies...')
+print('/////////////////////////////////')
+print(' ')
+os.system('sudo apt-get install python3-pandas')
+os.system('sudo apt-get install python3-pip')
+os.system('sudo pip3 install --upgrade setuptools')
+os.system('sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel')
+os.system('sudo python3 -m pip install --force-reinstall adafruit-blinka')
+os.system('sudo pip3 install RPi.GPIO')
+os.system('sudo pip3 install flask')
+
+print('\n/////////////////////////////////')
+print('Cloning Files From Git...')
+print('/////////////////////////////////')
+print(' ')
+
+# git doesn't like cloning into non-empty directories, so let's make sure this is empty first (in case of failed previous install?)
+try:
+	os.system('rm -rf ' + path + '/RaspiTwitchONAIR/')
+except:
+	pass
+
+os.system('git clone https://github.com/europaYuu/RaspiTwitchONAIR.git')
+os.system('rm -rf ' + path + '/RaspiTwitchONAIR/.git/')
+os.system('rm -rf ' + path + '/RaspiTwitchONAIR/.gitignore')
+print('\n/////////////////////////////////')
+print('Moving Files To /home/pi...')
+print('/////////////////////////////////')
+print(' ')
+os.system('mv -v ' + path + '/RaspiTwitchONAIR/* ' + path)
+os.system('rm -rf ' + path + '/RaspiTwitchONAIR/')
+
+print('\n/////////////////////////////////')
+print('Installing Services...')
+print('/////////////////////////////////')
+print(' ')
+os.system('sudo python3 twitch_install_services.py')
---
+++ twitch_update.py
@@ -0,0 +1,54 @@
+import os
+
+path = os.getcwd()
+
+print('\n/////////////////////////////////')
+print('Updating Twitch ON AIR Services...')
+print('/////////////////////////////////')
+print(' ')
+print('Current Path: ' + path)
+
+print('\n/////////////////////////////////')
+print('Performing apt-get update')
+print('/////////////////////////////////')
+print(' ')
+os.system('sudo apt-get update')
+
+print('\n/////////////////////////////////')
+print('Performing apt-get upgrade')
+print('/////////////////////////////////')
+print(' ')
+os.system('sudo apt-get upgrade')
+
+print('\n/////////////////////////////////')
+print('Installing Dependencies...')
+print('/////////////////////////////////')
+print(' ')
+os.system('sudo python3 twitch_install_dependencies')
+
+print('\n/////////////////////////////////')
+print('Cloning Files From Git...')
+print('/////////////////////////////////')
+print(' ')
+
+# git doesn't like cloning into non-empty directories, so let's make sure this is empty first (in case of failed previous install?)
+try:
+	os.system('rm -rf ' + path + '/RaspiTwitchONAIR/')
+except:
+	pass
+
+os.system('git clone https://github.com/europaYuu/RaspiTwitchONAIR.git')
+os.system('rm -rf ' + path + '/RaspiTwitchONAIR/.git/')
+os.system('rm -rf ' + path + '/RaspiTwitchONAIR/.gitignore')
+print('\n/////////////////////////////////')
+print('Moving Files To /home/pi...')
+print('/////////////////////////////////')
+print(' ')
+os.system('mv -v ' + path + '/RaspiTwitchONAIR/* ' + path)
+os.system('rm -rf ' + path + '/RaspiTwitchONAIR/')
+
+print('\n/////////////////////////////////')
+print('Rebooting...')
+print('/////////////////////////////////')
+print(' ')
+os.system('sudo reboot')
